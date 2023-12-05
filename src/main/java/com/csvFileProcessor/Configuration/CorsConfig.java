e8925b82a266742dd54248b5732e59a9013a78e6
Vijay Kumar Das <vijaydas0078@gmail.com>
2023-12-06 01:24:13 +0530

    keep server alive in particular time frame

---
+++ src/main/java/com/csvFileProcessor/Configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.csvFileProcessor.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://paytm--clone.vercel.app", "http://localhost:3000/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- src/main/java/com/csvFileProcessor/Controller/Controller.java
+++ src/main/java/com/csvFileProcessor/Controller/Controller.java
@@ -4,11 +4,13 @@ import com.csvFileProcessor.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @org.springframework.stereotype.Controller
+@CrossOrigin(origins = {"http://localhost:3000","https://paytm--clone.vercel.app"})
 public class Controller {
     @Autowired
     UserService userService;
@@ -18,5 +20,11 @@ public class Controller {
                                           @RequestParam("conditionValue") String conditionValue){
         String response = userService.getResult(file,conditionColumnName,conditionValue);
         return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @GetMapping (path = "/keepAlive")
+     public ResponseEntity<?> keepAlive()
+    {
+        return new ResponseEntity<>("You've hit the server to keep it alive" , HttpStatus.OK);
     }
 }
